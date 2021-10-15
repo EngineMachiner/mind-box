62cc2335d873c3482f316fbf0120bf77c717c287
EngineMachiner <mfpc98@gmail.com>
2021-10-15 00:21:21 -0300

    No metamethods or ctx

--- mind$box/mind$box.lua
+++ mind$box/mind$box.lua
@@ -11,7 +11,11 @@ local function IsEmpty( t, which )
 
 	local check
 	for k,v in pairs(t) do
-		if v then check = true break end
+		local s_k = tostring(k)
+		if v and not s_k:match("ctx")
+		and not s_k:match("__%a") then 
+			check = true break 
+		end
 	end
 
 	local s = which or "Start"
@@ -63,67 +67,73 @@ local function ConcatTable( ... )
 			local s_val = tostring(v)
 			local s_key = [["]] .. k .. [["]] .. " = "
 			s_key = type(k) == "number" and "" or s_key
-			
-			if type(v) == "table" then
 
-				if type(b4) ~= "table"
-				and i > 1 then
-					s.str = s.str .. "\n" .. s.spc
+			if not s_key:match("ctx") and not s_key:match("__%a") then
+
+				if type(v) == "table" then
+
+					if type(b4) ~= "table"
+					and i > 1 then
+						s.str = s.str .. "\n" .. s.spc
+					end
+
+					if smth ~= tbl_init then
+						s.str = s.str .. "\n" .. s.spc
+					end
+					s.str = s.str .. s_key
+					Reload(v)
+					
+					s_val = "}" .. last
+					if IsEmpty(v) == "\n" then
+						s.spc = s.spc:sub(1,#s.spc-4)
+						local spc = "\n" .. s.spc
+						s_val = spc .. s_val
+					end
+					s.Lim = ""
+
+					s.str = s.str .. s_val
+
+				else
+
+					if type(b4) == "table" then
+						s.str = s.str .. "\n\n" .. s.spc
+					end
+
+					s.Lim = s.Lim .. s_key
+					s.Lim = s.Lim .. s_val .. last
+
+					local a = "\n"
+					if #s.Lim <= lim then 
+						a = " "
+					else
+						s_key = i > 1 and s.spc .. s_key or s_key
+						s.Lim = s_key
+						s.Lim = s.Lim .. s_val .. last
+						if #s.Lim > lim then lim = #s.Lim end
+					end
+					
+					local nl = smth == former and "\n" or ""
+					s_key = i == 1 and nl .. s.spc .. s_key or s_key
+					
+					if a == " " then
+						s_val = s_val .. last .. a
+					else
+						s_key = i > 1 and a .. s_key or s_key
+						s_val = s_val .. last
+					end
+					s.str = s.str .. s_key .. s_val
+
 				end
 
-				if smth ~= tbl_init then
-					s.str = s.str .. "\n" .. s.spc
+				if i == lst_i and smth == former then
+					s.str = s.str .. "\n"
 				end
-				s.str = s.str .. s_key
-				Reload(v)
-				
-				s_val = "}" .. last
-				if IsEmpty(v) == "\n" then
-					s.spc = s.spc:sub(1,#s.spc-4)
-					local spc = "\n" .. s.spc
-					s_val = spc .. s_val
-				end
-				s.Lim = ""
 
-				s.str = s.str .. s_val
+				b4 = v
 
 			else
-
-				if type(b4) == "table" then
-					s.str = s.str .. "\n\n" .. s.spc
-				end
-
-				s.Lim = s.Lim .. s_key
-				s.Lim = s.Lim .. s_val .. last
-
-				local a = "\n"
-				if #s.Lim <= lim then 
-					a = " "
-				else
-					s_key = i > 1 and s.spc .. s_key or s_key
-					s.Lim = s_key
-					s.Lim = s.Lim .. s_val .. last
-					if #s.Lim > lim then lim = #s.Lim end
-				end
-				
-				local nl = smth == former and "\n" or ""
-				s_key = i == 1 and nl .. s.spc .. s_key or s_key
-				
-				if a == " " then
-					s_val = s_val .. last .. a
-				else
-					s_key = i > 1 and a .. s_key or s_key
-					s_val = s_val .. last
-				end
-				s.str = s.str .. s_key .. s_val
-
+				s.spc = s.spc:sub(1,#s.spc-4)
 			end
-
-			if i == lst_i and smth == former then
-				s.str = s.str .. "\n"
-			end
-
-			b4 = v
 
 	    end
 
